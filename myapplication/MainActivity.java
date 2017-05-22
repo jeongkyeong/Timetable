12bc40dcb7111e7f2c025f2bb08ca68428899620
SangKyu <jsk6387@naver.com>
2017-05-22 16:20:27 +0900

    test

--- myapplication/MainActivity.java
+++ myapplication/MainActivity.java
@@ -1,7 +1,12 @@
-package com.example.jungyun.myapplication;
+package myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editPW = (EditText) findViewById(R.id.edit_pw);
+
+    }
+    public void onClick(View view)
+    {
+        EditText editID = (EditText) findViewById(R.id.edit_id);
+        String id=editID.getText().toString();
+        Intent intent = new Intent(this,SubActivity.class);
+        intent.putExtra("ID", id);
+        startActivity(intent);
     }
 }
+
