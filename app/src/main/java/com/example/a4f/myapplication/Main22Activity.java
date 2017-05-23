6649256bfa357f5f68352689a6074e5e330e7bd2
jkkang <kangjk94@gmail.com>
2017-05-23 22:03:13 +0900

    main2.xml

--- app/src/main/java/com/example/a4f/myapplication/Main22Activity.java
+++ app/src/main/java/com/example/a4f/myapplication/Main22Activity.java
@@ -23,7 +23,7 @@ public class Main22Activity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*
+/*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
