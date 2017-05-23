ed7f23f8a7deda79bcb131676377040c19ed2cc1
christyeum0 <christyeum@naver.com>
2017-05-23 21:17:53 +0900

    button connected

--- app/src/main/java/com/example/a4f/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a4f/myapplication/MainActivity.java
@@ -1,5 +1,15 @@
 package com.example.a4f.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick1(View v) {
-        setContentView(R.layout.Main2Activity);
+        setContentView(R.layout.activity_main2);
     }
 
     public void onButtonClick2(View v) {
-        setContentView(R.layout.Main22Activity);
+        setContentView(R.layout.activity_main22);
     }
 }
