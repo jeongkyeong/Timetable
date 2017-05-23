2eca708ac496d26b29b8f968cfe377b5198d6cea
jkkang <kangjk94@gmail.com>
2017-05-24 01:39:22 +0900

    uiui

--- app/src/main/java/com/example/a4f/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/a4f/myapplication/Main2Activity.java
@@ -1,9 +1,18 @@
 package com.example.a4f.myapplication;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 
 public class Main2Activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/a4f/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a4f/myapplication/MainActivity.java
@@ -28,9 +28,9 @@ public class MainActivity extends AppCompatActivity {
         testDB();
     }
 
-    private static final String url = "jdbc:mysql://localhost:8080/test"; //test is name of your db
+    private static final String url = "jdbc:mysql://localhost:3306/test"; //test is name of your db
     private static final String user = "root";
-    private static final String pass = "1234";
+    private static final String pass = "123456";
 
     public void testDB(){
 
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
             String result = "Database connection success\n";
             Statement st = connection.createStatement();
-            ResultSet resultSet = st.executeQuery("select * from Persons");
+            ResultSet resultSet = st.executeQuery("select * from users");
             ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
 
             while(resultSet.next()){
