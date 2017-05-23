39fd718ced9d3fb0455328be87fa3a15123915ad
christyeum0 <christyeum@naver.com>
2017-05-23 22:40:26 +0900

    check mainactivity

--- app/src/main/java/com/example/a4f/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a4f/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a4f.myapplication;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,13 +19,46 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testDB();
     }
+
+    private static final String url = "jdbc:mysql://localhost:8080/test"; //test is name of your db
+    private static final String user = "root";
+    private static final String pass = "1234";
+
+    public void testDB(){
+
+        TextView textView = (TextView)this.findViewById(R.id.textView);
+        try{
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, pass);
+
+            String result = "Database connection success\n";
+            Statement st = connection.createStatement();
+            ResultSet resultSet = st.executeQuery("select * from Persons");
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+            while(resultSet.next()){
+                result += resultSetMetaData.getColumnName(1) + ":" + resultSet.getString(1) + "\n";
+                result += resultSetMetaData.getColumnName(2) + ":" + resultSet.getString(1) + "\n";
+
+            }
+            textView.setText(result);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            textView.setText(e.toString());
+        }
+    }
+
 
     public void onButtonClick1(View v) {
         setContentView(R.layout.activity_main2);
