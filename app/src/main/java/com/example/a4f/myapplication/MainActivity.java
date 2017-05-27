6e34c7e0d30fe7b23bd8467ffc57a9e849aa9e7b
SangKyu <jsk6387@naver.com>
2017-05-27 16:13:05 +0900

    Android login with php mysql

--- app/src/main/java/com/example/a4f/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a4f/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
@@ -21,11 +22,13 @@ import java.sql.Statement;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText userNameEt,passwordEt;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testDB();
+        userNameEt = (EditText)findViewById(R.id.etUserName);
+        passwordEt = (EditText)findViewById(R.id.etPassword);
+        //testDB();
     }
 
     private static final String url = "jdbc:mysql://localhost:3306/test"; //test is name of your db
@@ -66,11 +69,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
     }
 
-    public void onButtonClick2(View v) {
+    public void onLoginClick(View v) {
+        String username = userNameEt.getText().toString();
+        String password = passwordEt.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
         setContentView(R.layout.activity_main22);
     }
 
     public void onButtonClick3(View view) {
+
         setContentView(R.layout.activity_main);
     }
 }
