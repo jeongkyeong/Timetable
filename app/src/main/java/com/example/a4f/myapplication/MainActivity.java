e94aa996298765c93dbdce3ca52760e719f016b9
SangKyu <jsk6387@naver.com>
2017-05-28 01:41:03 +0900

    bug update

--- app/src/main/java/com/example/a4f/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a4f/myapplication/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         userNameEt = (EditText)findViewById(R.id.etUserName);
-        passwordEt = (EditText)findViewById(R.id.etRegId);
+        passwordEt = (EditText)findViewById(R.id.etPassword);
         //testDB();
     }
 
