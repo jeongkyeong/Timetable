a3f40e27bd508886096ffe62a0465dbfa81fe272
강정경 <kangjk94@gmail.com>
2017-05-31 22:32:59 +0900

    헤헤

---
+++ app/src/main/java/com/example/a4f/myapplication/MyAccount.java
@@ -0,0 +1,15 @@
+package com.example.a4f.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 강정경 on 2017-05-31.
+ */
+
+public class MyAccount extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myaccount);
+    }
+}
