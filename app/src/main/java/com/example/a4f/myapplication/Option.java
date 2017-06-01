26618fa1ea0caef2c8d6203bf0461675bbd5b8df
christyeum0 <christyeum@naver.com>
2017-06-01 15:04:29 +0900

    -

--- app/src/main/java/com/example/a4f/myapplication/Option.java
+++ app/src/main/java/com/example/a4f/myapplication/Option.java
@@ -1,9 +1,12 @@
 package com.example.a4f.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by 정윤 on 2017-05-31.
@@ -15,5 +18,21 @@ public class Option extends AppCompatActivity {
         setContentView(R.layout.option);
         setTitle("Filter Options");
 
+    }
+
+    public void onBackPressed() {
+        Intent i = new Intent(Option.this, NavActivity.class);
+        startActivity(i);
+        finish();
+    }
+    public void btnCancelClick(View view) {
+        Toast.makeText(Option.this, "Cancelled", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Option.this, NavActivity.class);
+        startActivity(i);
+    }
+    public void btnConfirmClick(View view) {
+        Toast.makeText(Option.this, "Confirmed", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Option.this, EntireTTActivity.class);
+        startActivity(i);
     }
 }
