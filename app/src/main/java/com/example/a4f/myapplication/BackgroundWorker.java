92ce13ad2bf8e60e584888e0b44666989ba06d58
jkkang <kangjk94@gmail.com>
2017-06-11 02:47:17 +0900

    Register 에서 아이디체크 비밀번호체크햇뉸데 ............아이디체크왜이상한걸까염 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- app/src/main/java/com/example/a4f/myapplication/BackgroundWorker.java
+++ app/src/main/java/com/example/a4f/myapplication/BackgroundWorker.java
@@ -42,6 +42,7 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
         String loginUrl= "http://timetable.dothome.co.kr/login.php";
         String regUrl = "http://timetable.dothome.co.kr/register.php";
         String editUrl = "http://timetable.dothome.co.kr/edit.php";
+        String IDcheckUrl = "http://timetable.dothome.co.kr/IDcheck.php";
 
         if(type.equals("login")) {      // send login information (id, pw) to apache server to check if it is ok.
             try {
@@ -81,6 +82,47 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 inputStream.close();
                 httpURLConnection.disconnect();
                 return check;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(type.equals("IDcheck")) {
+            try {
+                String idcheck="";
+                String ID = params[1];
+                URL url = new URL(IDcheckUrl);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+
+                //send register data to the apache.
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("ID", "UTF-8") + "=" + URLEncoder.encode(ID, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                if(result.equals("1")){
+                    idcheck = "1";
+                }
+                else{
+                    idcheck = "0";
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return idcheck;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
